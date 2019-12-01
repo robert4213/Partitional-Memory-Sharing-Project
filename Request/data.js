1d29bcb8dbedd735c00e5b032e8523de8f72d4a6
robert4213 <573679310@qq.com>
2019-12-01 00:03:43 -0800

    Create data.js

---
+++ Request/data.js
@@ -0,0 +1,19 @@
+var app_name = 'dropbox';
+
+function Data() {
+    // this.filename = '';
+    this.chunk = '';
+    this.hashId = '';
+    this.dataSize = '';
+
+    //Read Data from disk
+    this.readFIle = function (address){
+
+    }
+
+    //Save file to disk
+    this.saveFile = function (address , chunk) {
+
+    }
+
+}
